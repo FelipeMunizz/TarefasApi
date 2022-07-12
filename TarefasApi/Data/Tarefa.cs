8d3696f0c0a41c0b34735ce927be8fa87dbf68df
Felipe Muniz <lipe.baterra@gmail.com>
2022-07-11 21:19:53 -0300

    Adicionar arquivos de projeto.

---
+++ TarefasApi/Data/Tarefa.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TarefasApi.Data;
+
+[Table("Tarefas")]
+public record Tarefa(int Id, string Atividade, string Status);
+
+
+// Record é um novo recurso que foi introduzido no C# 9, ele é um tipo de dados leves e imutavel que tem propriedade somente leitura;
+// ele é seguro para threads e não pode sofrer alteração apos sua criação, ele só pode ser inicializado atraves de um construtor;
+// eles são tipo por referencia, que se comportam como tipos por valores no quesito comparação de igualdade
---
+++ TarefasApi/Program.cs
@@ -0,0 +1,11 @@
+using TarefasApi.Endpoints;
+using TarefasApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddPersistence();
+
+var app = builder.Build();
+
+app.MapTarefasEndPoints();
+
+app.Run();
